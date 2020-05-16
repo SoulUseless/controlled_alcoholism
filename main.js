dfef55269dcd8d5fd1802406bf2c5d4c7d65ec73
Yap Jing Kang <jkyap99@gmail.com>
2020-05-16 14:44:48 +0800

    On branch master  Your branch is up to date with 'origin/master'.
     - Added 2 new articles: Glendronach 15 and Maker's 46
     - Adjusted formatting for some pages
     - Fixed some faulty links
    
     Changes to be committed:
    	new file:   .htaccess
    	modified:   about.html
    	modified:   cocktails.html
    	new file:   css/bootstrap.min.css.map
    	modified:   css/style.css
    	modified:   index.html
    	new file:   main.js
    	new file:   raws/glendronach_15.txt
    	new file:   raws/makers_46.txt
    	modified:   whiskey.html
    	modified:   whisky/glendronach_15/glendronach_15.html
    	new file:   whisky/glendronach_15/images/distillery.jpg
    	new file:   whisky/glendronach_15/images/glendronach_core.jpg
    	modified:   whisky/hibiki_17/hibiki_17.html
    	new file:   whisky/makers_46/images/makers_lineup.jpg
    	new file:   whisky/makers_46/images/makers_mark.jpg
    	modified:   whisky/makers_46/makers_46.html

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/*
+document.addEventListener('DOMContentLoaded', () => {
+	const articleCards = document.querySelectorAll('.article-card');
+	articleCards.forEach(article => article.addEventListener('mouseover', () => {
+		article.style.backgroundColor = "rgba(255, 251, 255, 1)";
+		console.log("trig");
+	}));
+})
+*/
